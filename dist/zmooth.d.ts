ff5aa7592b7ace3e9974ef8bf329b2a6c9f4685d
nico b <bouvet.nicolas@gmail.com>
2023-02-22 14:52:14 +0100

    refactor for iife

--- dist/zmooth.d.ts
+++ dist/zmooth.d.ts
@@ -58,24 +58,24 @@ declare class ZmoothManager {
     val<T extends number | number[]>(value: T, speed?: number, onChange?: (value: T) => void): ZmoothArray | ZmoothNumber;
     /**
      * Identical to val()
-     * The difference is that the new value will be automatically assigned to the field in the object you passed in parameters
+     * The difference is that the new value will be automatically assigned to the property in the object you passed in parameters
      * @example
      * const myObj = {
      *     myValue: 0,
      * };
      *
-     * // now field 'myValue' on object myObj is managed automatically
-     * const myZmooth = zmooth.field(myObj, 'myValue');
+     * // now property 'myValue' on object myObj is managed automatically
+     * const myZmooth = zmooth.prop(myObj, 'myValue');
      *
      * // now myObj.value will automatically be interpolated to 10
      * myZmooth.to = 10;
      * @param obj Any javascript object
-     * @param fieldName Field that must be smoothed
+     * @param propertyName Property that must be smoothed
      * @param speed Speed of change
      * @param onChange Callback each time the value is updated
      * @returns
      */
-    prop<T extends Record<any, any>>(obj: T, fieldName: keyof T, speed?: number, onChange?: (value: any) => void): BaseZmooth<any>;
+    prop<T extends Record<any, any>>(obj: T, propertyName: keyof T, speed?: number, onChange?: (value: any) => void): BaseZmooth<any>;
     /**
      * Kill a zmooth object
      * @param zmooth zmooth object to kill
@@ -94,7 +94,7 @@ declare class ZmoothManager {
 declare const _default: {
     inst: (autoUpdate?: boolean) => ZmoothManager;
     val: <T extends number | number[]>(value: T, speed?: number | undefined, onChange?: ((value: T) => void) | undefined) => ZmoothArray | ZmoothNumber;
-    prop: <T_1 extends Record<any, any>>(obj: T_1, fieldName: keyof T_1, speed?: number | undefined, onChange?: ((value: any) => void) | undefined) => BaseZmooth<any>;
+    prop: <T_1 extends Record<any, any>>(obj: T_1, propertyName: keyof T_1, speed?: number | undefined, onChange?: ((value: any) => void) | undefined) => BaseZmooth<any>;
     killAll: () => void;
 };
 
--- tsup.config.ts
+++ tsup.config.ts
@@ -1,7 +1,12 @@
+import fs from 'fs';
 import { defineConfig } from 'tsup';
 
 export default defineConfig((options) => {
-    return {
+    if (!options.minify) {
+        fs.rmSync('./dist', { recursive: true, force: true });
+    }
+
+    const config = {
         entry: options.minify
             ? {
                   'zmooth.min': 'src/index.ts',
@@ -9,19 +14,31 @@ export default defineConfig((options) => {
             : {
                   zmooth: 'src/index.ts',
               },
-        clean: !options.minify,
+        clean: false,
         dts: true,
         sourcemap: options.watch,
-        format: ['cjs', 'esm'],
+        bundling: !options.watch,
         platform: 'browser',
-        // outExtension({ format }) {
-        //     if (format === 'cjs') return { js: `.cjs` };
-        //     else if (format === 'esm') return { js: `.mjs` };
-        //     else if (format === 'iife') return { js: `.js` };
-
-        //     return {
-        //         js: `.${format}.js`,
-        //     };
-        // },
+        outExtension({ format }) {
+            if (format === 'cjs') return { js: `.cjs` };
+            else if (format === 'esm') return { js: `.mjs` };
+            else if (format === 'iife') return { js: `.js` };
+            return { js: `.${format}.js` };
+        },
     };
+
+    return [
+        {
+            ...config,
+            format: ['cjs', 'esm'],
+        },
+        {
+            ...config,
+            format: ['iife'],
+            globalName: 'zmooth',
+            footer: {
+                js: 'zmooth = zmooth.default;',
+            },
+        },
+    ];
 });
